932b09e85c3c4d645be1228b48bf47e95328b5af
mike hill <michael-hill@lambdastudents.com>
2021-08-22 09:17:05 -0400

    wrote tests for app.js

---
+++ weatherapp/src/App.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import App from './App'
+import userEvent from '@testing-library/user-event'
+
+
+const mockGeolocation = {
+    getCurrentPosition: jest.fn()
+        .mockImplementationOnce((success) => Promise.resolve(success({
+            coords: {
+                latitude: 51.1,
+                longitude: 45.3
+            }
+        })))
+};
+global.navigator.geolocation = mockGeolocation;
+
+describe('testing app renders header and nav', () => {
+
+    test('app renders without errors', async () => {
+        render(<App />)
+    })
+
+    test('Checking for header rendering', async () => {
+        render(<App />)
+        const modal = await screen.findAllByText(/Allow Access to Location/i)
+        const allow = await screen.findByTestId(/allowBtn/i)
+        await userEvent.click(allow)
+
+        const nav = await screen.findAllByText(/Todays Weather/i)
+        const Title = await screen.findAllByText(/The best weather ever/i)
+    })
+})
